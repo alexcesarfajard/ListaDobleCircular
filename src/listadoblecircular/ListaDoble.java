13cb9bc20ea4136ee5d143e8c84240d7562c0c96
alexcesarfajard <acesar23616@ufide.ac.cr>
2023-11-01 20:07:48 -0600

    se agrega existe

--- src/listadoblecircular/ListaDoble.java
+++ src/listadoblecircular/ListaDoble.java
@@ -9,70 +9,89 @@ package listadoblecircular;
  * @author Usuario
  */
 public class ListaDoble {
-    
+
     private Nodo cabeza;
     private Nodo ultimo;
-    
-    public void inserta(Persona p){
-        
-        if (cabeza == null){
+
+    public void inserta(Persona p) {
+
+        if (cabeza == null) {
             cabeza = new Nodo(p);
             ultimo = cabeza;
             ultimo.setSiguiente(cabeza);
-            ultimo.setAnterior(ultimo); 
+            ultimo.setAnterior(ultimo);
         } else {
-            if( cabeza.getDato().getId() > p.getId() ) {
+            if (cabeza.getDato().getId() > p.getId()) {
                 Nodo aux = new Nodo(p);
-                
+
                 aux.setSiguiente(cabeza);
                 cabeza = aux;
                 ultimo.setSiguiente(cabeza);
-                cabeza.setAnterior(ultimo);    
+                cabeza.setAnterior(ultimo);
             } else {
-                if(p.getId() > ultimo.getDato().getId()){
+                if (p.getId() > ultimo.getDato().getId()) {
                     ultimo.setSiguiente(new Nodo(p));
                     ultimo = ultimo.getSiguiente();
                     ultimo.setSiguiente(cabeza);
                     cabeza.setAnterior(ultimo);
                 } else {
                     Nodo aux = cabeza;
-                    
-                    while (aux.getSiguiente().getDato().getId() < p.getId()) {                        
+
+                    while (aux.getSiguiente().getDato().getId() < p.getId()) {
                         aux = aux.getSiguiente();
                     }
-                    
+
                     Nodo temp = new Nodo(p);
                     temp.setSiguiente(aux.getSiguiente());
                     temp.setAnterior(temp);
                     aux.setSiguiente(temp);
                     temp.getSiguiente().setAnterior(temp);
-                    
+
                 }
             }
         }
-        
+
     }
-    
-    public String toString(){
+
+    public boolean existe(int id) {
+        boolean existe = false;
+
+        if (cabeza != null) {
+            if (id >= cabeza.getDato().getId() && id <= cabeza.getDato().getId()) {
+                
+                Nodo aux = cabeza;
+
+                while (aux != ultimo && aux.getDato().getId() <= id) {
+                    aux = aux.getSiguiente();
+                }
+                existe = (aux.getDato().getId() == id);
+            }
+        } else {
+            System.out.println("La lista está vacía");
+        }
+
+        return existe;
+    }
+
+    public String toString() {
         String respuesta = "Lista doble circular \n";
-        
-        if(cabeza != null) {
+
+        if (cabeza != null) {
             Nodo aux = cabeza;
-            
+
             respuesta += aux.toString() + "\n";
             aux = aux.getSiguiente();
-            
-            while (aux != cabeza) {                
+
+            while (aux != cabeza) {
                 respuesta += aux.toString() + "\n";
                 aux = aux.getSiguiente();
             }
-            
-            
+
         } else {
             respuesta += "La lista está vacía";
         }
-        
+
         return respuesta;
     }
-    
+
 }
--- src/listadoblecircular/ListaDobleCircular.java
+++ src/listadoblecircular/ListaDobleCircular.java
@@ -5,7 +5,19 @@ public class ListaDobleCircular {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        ListaDoble lista = new ListaDoble();
         
+        lista.inserta(new Persona(2, "Alex"));
+        
+        System.out.println(lista.toString());
+        
+        lista.inserta(new Persona(1, "Juan"));
+        lista.inserta(new Persona(4, "Alberto"));
+        lista.inserta(new Persona(3, "Cesar"));
+        
+        System.out.println(lista.toString());
+        
+        System.out.println(lista.existe(2));
         
     }
     
